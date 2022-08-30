e8614f0f528c4e29c7530154dee098b0a77719a1
joredelma <78234275+joredelma@users.noreply.github.com>
2022-08-30 16:17:56 -0400

    summary

--- Ingest_Data/zipreadline.py
+++ Ingest_Data/zipreadline.py
@@ -1,10 +1,10 @@
-
+import csv
 import zipfile
 
 # import required module
 import os
 
-def main():
+def read2000():
     # assign directory
     directory = '//wsl$/Ubuntu/home/benjaminserio/P3_Census/'
     # iterate over files in
@@ -16,10 +16,28 @@ def main():
             for name in root.namelist():
                 print(name)
                 print(root.open(name).readline())
-                root.close()
+            root.close()
             # checking if it is a file
             if os.path.isfile(f):
                 print(f)
 
+def read2010():
+    # assign directory
+    directory = '//wsl$/Ubuntu/home/joredelma/2010data/'
+    # iterate over files in
+    # that directory
+    for filename in os.listdir(directory):
+        if "2010" in filename:
+            f = os.path.join(directory, filename)
+            root =  zipfile.ZipFile(f, "r")
+            for name in root.namelist():
+                if not "packing" in name and not "geo" in name:
+                    s = str(root.open(name).readline().decode('utf-8'))
+                    with open(os.path.join(directory, "line1"), 'a', encoding='utf-8') as csv_file:
+                        csv_file.write(s)
+            root.close()
+
+
 if __name__ == "__main__":
-    main()
+    #read2000()
+    read2010()
