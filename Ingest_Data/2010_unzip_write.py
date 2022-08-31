94695e4144867026db02e46b3529ab1cc9a100b4
justinchore <jchoustin91@gmail.com>
2022-08-31 13:15:54 -0700

    S3 Bucket Upload 2010 Files -> Unzip, Create in Local FileSystem, Upload into Bucket, Delete From Local FS

--- Ingest_Data/2010_unzip_write.py
+++ Ingest_Data/2010_unzip_write.py
@@ -1,15 +1,12 @@
-
-import zipfile
-
 # import required module
 import os
+import zipfile
+import upload_s3
 
-print("YO")
-
-pl1_header_file = '2010_Header_Files/2010-PL1Header.csv'
-pl1_data_file = '//wsl$/Ubuntu/home/jcho/census_data/2010-PL1Data.csv'
-pl2_header_file = '2010_Header_Files/2010-PL2Header.csv'
-pl2_data_file = '//wsl$/Ubuntu/home/jcho/census_data/2010-PL2Data.csv'
+pl1_header_file = '../2010-1Header.csv'
+pl1_data_file = '../2010_1.csv'
+pl2_header_file = '../2010-2Header.csv'
+pl2_data_file = '../2010_2.csv'
 
 
 def main():
@@ -18,21 +15,29 @@ def main():
     # iterate over files in
     # that directory
     initial_create()
+
     for filename in sorted(os.listdir(directory)):
         f = os.path.join(directory, filename)
         root = zipfile.ZipFile(f, "r")
+     
         for name in root.namelist():
             if "packinglist" not in name and "geo" not in name:
                 line = root.open(name).readline().decode('utf-8')
+        
                 if '12' in name:
                     append_line(line, 1)    
                 if '22' in name:
                     append_line(line, 2)
                     
         root.close()
-        # checking if it is a file
-        if os.path.isfile(f):
-            print(f)
+            
+    upload_s3.upload_file_s3(pl1_data_file)
+    os.remove(pl1_data_file)
+    
+    upload_s3.upload_file_s3(pl2_data_file)
+    os.remove(pl2_data_file)
+    
+    
 
 def append_line(line, plNumber):
     if plNumber == 1:
@@ -49,12 +54,14 @@ def initial_create():
         
     with open(pl1_data_file, 'w') as f2:
         f2.write(header)
+        f2.write('\n')
     
     with open(pl2_header_file, 'r') as f3:
         header = f3.readline()
         
     with open(pl2_data_file, 'w') as f4:
         f4.write(header)
+        f4.write('\n')
 
 if __name__ == "__main__":
     main()
---
+++ Startup.md
@@ -0,0 +1,6 @@
+# Web Crawler Script for US Census Redistricting Data 
+1. 
+
+
+# Data Injestion Application for 2010
+1. 
