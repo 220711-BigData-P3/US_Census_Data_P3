abfab209d17bd2cd68d7f01f419d82b8a2cf54f4
Phai <phai597@revature.net>
2022-08-31 12:51:42 -0400

    Finished querying highest population region for 2010

--- query_data/phai/pop_by_region.py
+++ query_data/phai/pop_by_region.py
@@ -121,5 +121,88 @@ file.close()
 #  `---'              `---`                     `---`      
 #                                                          
 
+file_name = "2010_1.csv"
+rdd_2010 = (
+    spark.read.option("header", True)
+    .option("inferSchema", True)
+    .csv(_filepath + file_name)
+)
+#######################################################################################
+#                                                                                     #
+#                                                                                     #
+#                   QUERYING EACH INDIVIDUAL REGION                                   #
+#                                                                                     #
+#                                                                                     #
+#######################################################################################
+
+data_view = rdd_2010.createOrReplaceTempView("2000_all")
+midwest_df = rdd_2010.filter((rdd_2010.STUSAB == "IA") | (rdd_2010.STUSAB == "KS") | (rdd_2010.STUSAB == "MO")| (rdd_2010.STUSAB == "NE")| (rdd_2010.STUSAB == "ND") | (rdd_2010.STUSAB == "SD") | (rdd_2010.STUSAB == "IL") | (rdd_2010.STUSAB == "IN") | (rdd_2010.STUSAB == "MI") | (rdd_2010.STUSAB == "MN") | (rdd_2010.STUSAB == "OH") | (rdd_2010.STUSAB == "WI"))
+midwest_view = midwest_df.createOrReplaceTempView("midwest_view")
+northeast_df = rdd_2010.filter((rdd_2010.STUSAB == "CT") | (rdd_2010.STUSAB == "DE") | (rdd_2010.STUSAB == "MA") | (rdd_2010.STUSAB == "ME") | (rdd_2010.STUSAB == "NH") | (rdd_2010.STUSAB == "NJ") | (rdd_2010.STUSAB == "NY") | (rdd_2010.STUSAB == "PA") | (rdd_2010.STUSAB == "RI") | (rdd_2010.STUSAB == "VT"))
+northeast_view = northeast_df.createOrReplaceTempView("northeast_view")
+southeast_df = rdd_2010.filter((rdd_2010.STUSAB == "AL") | (rdd_2010.STUSAB == "AR") | (rdd_2010.STUSAB == "FL") | (rdd_2010.STUSAB == "GA") | (rdd_2010.STUSAB == "KY") | (rdd_2010.STUSAB == "LA") | (rdd_2010.STUSAB == "MD") | (rdd_2010.STUSAB == "MS") | (rdd_2010.STUSAB == "NC") | (rdd_2010.STUSAB == "SC") | (rdd_2010.STUSAB == "TN") | (rdd_2010.STUSAB == "VA") | (rdd_2010.STUSAB == "WV"))
+southeast_view = southeast_df.createOrReplaceTempView("southeast_view")
+southwest_df = rdd_2010.filter((rdd_2010.STUSAB == "AZ") | (rdd_2010.STUSAB == "CA") | (rdd_2010.STUSAB == "CO") | (rdd_2010.STUSAB == "NV") | (rdd_2010.STUSAB == "NM") | (rdd_2010.STUSAB == "OK") | (rdd_2010.STUSAB == "TX") | (rdd_2010.STUSAB == "UT"))
+southwest_view = southwest_df.createOrReplaceTempView("southwest_view")
+west_df = rdd_2010.filter((rdd_2010.STUSAB == "AK") | (rdd_2010.STUSAB == "ID") | (rdd_2010.STUSAB == "MT") | (rdd_2010.STUSAB == "WY") | (rdd_2010.STUSAB == "WA") | (rdd_2010.STUSAB == "OR") | (rdd_2010.STUSAB == "HI"))
+west_view = west_df.createOrReplaceTempView("west_view")
+
+midwest_pop = spark.sql("SELECT SUM(P0010001) AS total_population FROM midwest_view")
+northeast_pop = spark.sql("SELECT SUM(P0010001) AS total_population FROM northeast_view")
+southeast_pop = spark.sql("SELECT SUM(P0010001) AS total_population FROM southeast_view")
+southwest_pop = spark.sql("SELECT SUM(P0010001) AS total_population FROM southwest_view")
+west_pop = spark.sql("SELECT SUM(P0010001) AS total_population FROM west_view")
+
+#######################################################################################
+#                                                                                     #
+#                                                                                     #
+#                 COMBINING THE INDIVIDUAL QUERIES TO DF                              #
+#                                                                                     #
+#                                                                                     #
+#######################################################################################
+
+midwest_final = midwest_pop.withColumn("region", lit("midwest"))
+northeast_final = northeast_pop.withColumn("region", lit("northeast"))
+southeast_final = southeast_pop.withColumn("region", lit("southeast"))
+southwest_final = southwest_pop.withColumn("region", lit("southwest"))
+west_final = west_pop.withColumn("region", lit("west"))
+
+all_regions_2010 = sc.union([northeast_final.rdd, midwest_final.rdd, southeast_final.rdd, southwest_final.rdd, west_final.rdd]).toDF()
+all_regions_view = all_regions_2010.createOrReplaceTempView("all_regions_view")
+all_regions_2010_query = spark.sql("SELECT region, total_population FROM all_regions_view")
+all_regions_2010_query.show()
+highest_pop_2010 = spark.sql("SELECT region, total_population FROM all_regions_view WHERE total_population = (SELECT MAX(total_population) FROM all_regions_view)")
+highest_pop_2010.show()
+
+#######################################################################################
+#                                                                                     #
+#                                                                                     #
+#                               WRITING TO FILE                                       #
+#                                                                                     #
+#                                                                                     #
+#######################################################################################
+
+file = open("pop_by_region_2010.csv", "w")
+file.write(f"midwest,{midwest_pop.collect()[0]['total_population']}\n")
+file.write(f"northeast,{northeast_pop.collect()[0]['total_population']}\n")
+file.write(f"southeast,{southeast_pop.collect()[0]['total_population']}\n")
+file.write(f"southwest,{southwest_pop.collect()[0]['total_population']}\n")
+file.write(f"west,{west_pop.collect()[0]['total_population']}\n")
+file.write(f"max,{highest_pop_2010.collect()[0]['region']}")
+file.close()
+
+
+#   .----------------.  .----------------.  .----------------.  .----------------. 
+#  | .--------------. || .--------------. || .--------------. || .--------------. |
+#  | |    _____     | || |     ____     | || |    _____     | || |     ____     | |
+#  | |   / ___ `.   | || |   .'    '.   | || |   / ___ `.   | || |   .'    '.   | |
+#  | |  |_/___) |   | || |  |  .--.  |  | || |  |_/___) |   | || |  |  .--.  |  | |
+#  | |   .'____.'   | || |  | |    | |  | || |   .'____.'   | || |  | |    | |  | |
+#  | |  / /____     | || |  |  `--'  |  | || |  / /____     | || |  |  `--'  |  | |
+#  | |  |_______|   | || |   '.____.'   | || |  |_______|   | || |   '.____.'   | |
+#  | |              | || |              | || |              | || |              | |
+#  | '--------------' || '--------------' || '--------------' || '--------------' |
+#   '----------------'  '----------------'  '----------------'  '----------------' 
+
 
 spark.stop()
