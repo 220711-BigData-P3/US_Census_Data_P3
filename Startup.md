729c97caaa9efb29a9320d1e28c3f1d0c97b62b8
jnesteruck <jan9499@gmail.com>
2022-09-07 14:17:17 -0700

    Updated startup documentation, Data Analysis section in progress

--- Startup.md
+++ Startup.md
@@ -110,11 +110,180 @@ python Ingest_Data/2020_ingest.py
 ## Data Analysis
 
 ### Goals
-1. Placeholder goal
+1. Determine what data is needed for each data analysis question.
+    * Determine if any records are unnecessary and use filters to remove any such records.
+    * Determine what columns (i.e. population data) are needed for each question.
+2. Write queries to select the desired data for each data analysis question.
+3. Use data visualization tools to produce charts, graphs, tables, etc. to visually present the data.
+
 ### Steps Below
-* [Table of content item placeholder](#placeholder-link)
+* [Create SparkSession](#creating-the-sparksession)
+* [Import, Filter, and Combine the Data](#importing-filtering-and-combining-the-data)
+* [Query the Data](#querying-the-requested-data)
+* [Create Data Visualizations](#creating-data-visualizations)
 
+## Creating the SparkSession
 
+Make sure that the PySpark module is installed on your system.
 
+```
+pip install pyspark
+```
 
+Import SparkSession from the pyspark.sql module.
 
+```python
+from pyspark.sql import SparkSession
+```
+
+Then use the SparkSession builder to create your SparkSession. Set the SparkContext variable as well.
+
+```python
+spark = SparkSession.builder \
+    .master("local") \
+    .appName("project3") \
+    .getOrCreate()
+
+sc = spark.sparkContext
+sc.setLogLevel("WARN")
+
+'''
+
+<SPARK JOBS HERE>
+
+'''
+
+# DON'T FORGET TO STOP THE SPARKSESSION!!
+spark.stop()
+```
+
+## Importing, Filtering, and Combining the Data
+
+1. To query from the dataset, we must first import the data we want to use. Use the following code to do this for each census year:
+
+```python
+path = # LOCATION OF FILES ON YOUR LOCAL MACHINE
+
+popdata20XX = spark.read \
+    .option("header", True) \
+    .option("inferSchema", True) \
+    .csv(path + "20XX_1.csv")
+```
+NOTE: For 2020, the file convention is 'YYYY_P1.csv', rather than 'YYYY_1.csv'.
+
+2. For the purposes of this project, we will not be including Puerto Rico in the national population data, nor will we be using the US national summary files or records. We will
+filter out this data as follows:
+
+```python
+# ADD YEAR COLUMN TO EACH DATAFRAME TO DIFFERENTIATE BETWEEN CENSUS YEAR
+# lit() FUNCTION CREATES COLUMN OF CONSTANT VALUES
+from pyspark.sql.functions import lit
+
+popdata20XX_raw.createOrReplaceTempView("popdata20XX_raw")
+popdata20XX = spark.sql("SELECT * FROM popdata20XX_raw WHERE STUSAB != 'PR' AND STUSAB != 'US'") \
+    .withColumn("Year", lit(20XX))
+```
+
+3. Finally, we must combine the data from each census into one DataFrame.
+
+```python
+popdata = popdata2000.union(popdata2010).union(popdata2020).repartition(1)
+# BE SURE TO REPARTITION, OR SAVED DATAFRAMES WILL BE SEPARATED BY CENSUS YEAR
+popdata.createOrReplaceTempView("popdata")
+```
+
+## Querying the Requested Data
+
+For this example, we'll refer to the question of population of different racial/ethnic categories. For the full query code, refer to the categoryQueries python file
+(Location: /query_data/byCategory/categoryQueries.py).  
+  
+Code for other queries are also available in the /query_data directory.  
+  
+Additionally, in this example, we'll use the spark.sql() function provided by pyspark to query results.
+
+1. First, we want to query the sum totals for various categories that are provided to us in the dataset.  
+The relevant categories will be:
+    * Total Population (P0010001)
+    * Population of One Race (P0010002)
+    * White Alone (P0010003)
+    * Black or African American Alone (P0010004)
+    * American Indian or Alaska Native Alone (P0010005)
+    * Asian Alone (P0010006)
+    * Native Hawaiian or Pacific Islander Alone (P0010007)
+    * Some Other Race Alone (P0010008)
+    * Two or More Races (P0010009)
+    * Hispanic of Any Race (P0020002)
+    * Non-Hispanic of Any Race (P0020003)
+    <br /><br />
+2. The next step is to write the queries to provide our desired results, the sum totals of the relevant categories. We will do this in two queries for each table.
+<br /><br />  
+
+The first query will look like this in our code, grabbing the state-by-state totals of each category and creating a temporary view:
+```python
+spark.sql("SELECT Year, STUSAB AS State, P0010001 AS Total, P0010002 AS OneRace, P0010003 AS White, P0010004 AS Black, "
+          "P0010005 AS NativeAm, P0010006 AS Asian, P0010007 AS PacIslander, P0010008 AS Other, P0010009 AS TwoOrMore, "
+          "P0020002 AS Hispanic, P0020003 AS NonHispanic FROM popdata").createOrReplaceTempView("cat_1")
+```
+
+Then, from the temporary view, we will query the sums of each category by year.
+```python
+usData_1 = spark.sql("SELECT Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, "
+                     "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
+                     "SUM(TwoOrMore) AS TwoOrMore, SUM(Hispanic) AS Hispanic, SUM(NonHispanic) AS NonHispanic FROM cat_1 "
+                     "GROUP BY Year")
+```
+
+For this query, we wanted to look at these categories from all angles, so there were various tables we decided to query.
+```python
+    # --------------- usData_2 (Non-Hispanic Totals for all categories, Hispanic as separate category) --------------- #
+spark.sql("SELECT Year, STUSAB AS State, P0010001 AS Total, P0020002 AS Hispanic, P0020005 AS White, P0020006 AS Black, P0020007 AS NativeAm, "
+          "P0020008 AS Asian, P0020009 AS PacIslander, P0020010 AS Other, P0020011 AS TwoOrMore FROM popdata") \
+          .createOrReplaceTempView("cat_2")
+
+usData_2 = spark.sql("SELECT Year, SUM(Total) AS Total, SUM(Hispanic) AS Hispanic, SUM(White) AS White, SUM(Black) AS Black, SUM(NativeAm) AS NativeAm, "
+                     "SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, SUM(TwoOrMore) AS TwoOrMore FROM cat_2 "
+                     "GROUP BY Year")
+
+    # --------------- hispUS (Hispanic Totals for all categories) --------------- #
+spark.sql("SELECT Year, STUSAB AS State, P0020002 AS Total, P0010002-P0020004 AS OneRace, P0010003-P0020005 AS White, P0010004-P0020006 AS Black, "
+          "P0010005-P0020007 AS NativeAm, P0010006-P0020008 AS Asian, P0010007-P0020009 AS PacIslander, P0010008-P0020010 AS Other, "
+          "P0010009-P0020011 AS TwoOrMore FROM popdata ORDER BY Total DESC").createOrReplaceTempView("hisp")
+
+hispUS = spark.sql("SELECT Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, SUM(NativeAm) AS NativeAm, "
+                   "SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, SUM(TwoOrMore) AS TwoOrMore FROM hisp "
+                   "GROUP BY Year")
+
+    # --------------- nonhispUS (Non-Hispanic Totals for all categories) --------------- #
+spark.sql("SELECT Year, STUSAB AS State, P0020003 AS Total, P0020004 AS OneRace, P0020005 AS White, P0020006 AS Black, P0020007 AS NativeAm, "
+          "P0020008 AS Asian, P0020009 AS PacIslander, P0020010 AS Other, P0020011 AS TwoOrMore FROM popdata ORDER BY Total DESC") \
+          .createOrReplaceTempView("nonhisp")
+
+nonhispUS = spark.sql("SELECT Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, SUM(NativeAm) AS NativeAm, "
+                      "SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, SUM(TwoOrMore) AS TwoOrMore FROM nonhisp "
+                      "GROUP BY Year")
+```
+
+2. To verify that you have your desired DataFrame, simply call the show function.
+```python
+usData_1.show()
+```
+
+3. Now that we have our desired DataFrames, we can use the DataFrame.write function to save them to CSV files.
+```python
+savepath = # FILEPATH YOU WANT TO SAVE TO
+usData_1.write.csv(savepath + "usData_1", header=True)
+```
+This function will create a CSV file for each partition in the DataFrame and save it to a directory entitle "usData_1". In this case, we have one partition, so there should only be one CSV files. The remaining files are not important for our project, so they can be discarded.
+
+4. Submit the program as a spark job using the ```spark-submit``` command within the Ubuntu terminal.
+```
+spark-submit categoryQueries.py
+```
+As a result, you should see a new folder in your specified directory containing 4 files.
+<br />
+![Screenshot of directory and files created by DataFrame.write.csv() function.](documentation_screenshots/data_analysis_docs/DFcsv_files.png "Files generated by the write.csv function")
+
+You can rename the part-00000[...].csv file for convenience when creating data visualizations.
+
+## Creating Data Visualizations
+<!--UNDER CONSTRUCTION-->
--- query_data/byCategory/categoryQueries.py
+++ query_data/byCategory/categoryQueries.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import format_number, lit
+from pyspark.sql.functions import lit
 
 # ---------- BUILD SPARK SESSION ---------- #
 spark = SparkSession.builder \
@@ -17,147 +17,70 @@ path = mypath + "US_Census_Data_P3/"
 
 # ---------- CREATE DATAFRAMES FROM FILES ---------- #
     # ----- 2020 ----- #
-popdata2020 = spark.read \
+popdata2020_raw = spark.read \
     .option("header", True) \
     .option("inferSchema", True) \
     .csv(path + "2020_P1.csv")
 
     # ----- 2010 ----- #
-popdata2010 = spark.read \
+popdata2010_raw = spark.read \
     .option("header", True) \
     .option("inferSchema", True) \
     .csv(path + "2010_1.csv")
 
     # ----- 2000 ----- #
-popdata2000 = spark.read \
+popdata2000_raw = spark.read \
     .option("header", True) \
     .option("inferSchema", True) \
     .csv(path + "2000_1.csv")
 
 # REMOVE NATIONAL SUMMARY AND PUERTO RICO FROM DATA
-popdata2020.createOrReplaceTempView("popdata2020_ex")
-spark.sql("SELECT * FROM popdata2020_ex WHERE STUSAB != 'PR' AND STUSAB != 'US'").withColumn("Year", lit(2020)).createOrReplaceTempView("popdata2020")
-popdata2010.createOrReplaceTempView("popdata2010_ex")
-spark.sql("SELECT * FROM popdata2010_ex WHERE STUSAB != 'PR' AND STUSAB != 'US'").withColumn("Year", lit(2010)).createOrReplaceTempView("popdata2010")
-popdata2000.createOrReplaceTempView("popdata2000_ex")
-spark.sql("SELECT * FROM popdata2000_ex WHERE STUSAB != 'PR' AND STUSAB != 'US'").withColumn("Year", lit(2000)).createOrReplaceTempView("popdata2000")
+popdata2020_raw.createOrReplaceTempView("popdata2020_raw")
+popdata2020 = spark.sql("SELECT * FROM popdata2020_raw WHERE STUSAB != 'PR' AND STUSAB != 'US'").withColumn("Year", lit(2020))
+popdata2010_raw.createOrReplaceTempView("popdata2010_raw")
+popdata2010 = spark.sql("SELECT * FROM popdata2010_raw WHERE STUSAB != 'PR' AND STUSAB != 'US'").withColumn("Year", lit(2010))
+popdata2000_raw.createOrReplaceTempView("popdata2000_raw")
+popdata2000 = spark.sql("SELECT * FROM popdata2000_raw WHERE STUSAB != 'PR' AND STUSAB != 'US'").withColumn("Year", lit(2000))
 
-# ---------------------------------------- 2020 DATA ---------------------------------------- #
-    # --------------- usData2020_1 (Totals for all categories) --------------- #
+popdata = popdata2000.union(popdata2010).union(popdata2020).repartition(1)
+popdata.createOrReplaceTempView("popdata")
+
+# ---------------------------------------- QUERIES ---------------------------------------- #
+    # --------------- usData_1 (Totals for all categories) --------------- #
 spark.sql("SELECT Year, STUSAB AS State, P0010001 AS Total, P0010002 AS OneRace, P0010003 AS White, P0010004 AS Black, "
           "P0010005 AS NativeAm, P0010006 AS Asian, P0010007 AS PacIslander, P0010008 AS Other, P0010009 AS TwoOrMore, "
-          "P0020002 AS Hispanic, P0020003 AS NonHispanic FROM popdata2020 ORDER BY Total DESC").createOrReplaceTempView("cat2020_1")
+          "P0020002 AS Hispanic, P0020003 AS NonHispanic FROM popdata").createOrReplaceTempView("cat_1")
 
-usData2020_1 = spark.sql("SELECT MIN(Year) AS Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, "
-                         "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
-                         "SUM(TwoOrMore) AS TwoOrMore, SUM(Hispanic) AS Hispanic, SUM(NonHispanic) AS NonHispanic FROM cat2020_1")
+usData_1 = spark.sql("SELECT Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, SUM(NativeAm) AS NativeAm, "
+                     "SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, SUM(TwoOrMore) AS TwoOrMore, "
+                     "SUM(Hispanic) AS Hispanic, SUM(NonHispanic) AS NonHispanic FROM cat_1 GROUP BY Year")
 
-    # --------------- usData2020_2 (Non-Hispanic Totals for all categories, Hispanic as separate category) --------------- #
+    # --------------- usData_2 (Non-Hispanic Totals for all categories, Hispanic as separate category) --------------- #
 spark.sql("SELECT Year, STUSAB AS State, P0010001 AS Total, P0020002 AS Hispanic, P0020005 AS White, P0020006 AS Black, P0020007 AS NativeAm, "
-          "P0020008 AS Asian, P0020009 AS PacIslander, P0020010 AS Other, P0020011 AS TwoOrMore FROM popdata2020 ORDER BY Total DESC") \
-          .createOrReplaceTempView("cat2020_2")
+          "P0020008 AS Asian, P0020009 AS PacIslander, P0020010 AS Other, P0020011 AS TwoOrMore FROM popdata") \
+          .createOrReplaceTempView("cat_2")
 
-usData2020_2 = spark.sql("SELECT MIN(Year) AS Year, SUM(Total) AS Total, SUM(Hispanic) AS Hispanic, SUM(White) AS White, SUM(Black) AS Black, "
-                         "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
-                         "SUM(TwoOrMore) AS TwoOrMore FROM cat2020_2")
+usData_2 = spark.sql("SELECT Year, SUM(Total) AS Total, SUM(Hispanic) AS Hispanic, SUM(White) AS White, SUM(Black) AS Black, SUM(NativeAm) AS NativeAm, "
+                     "SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, SUM(TwoOrMore) AS TwoOrMore FROM cat_2 "
+                     "GROUP BY Year")
 
-    # --------------- hispUS2020 (Hispanic Totals for all categories) --------------- #
+    # --------------- hispUS (Hispanic Totals for all categories) --------------- #
 spark.sql("SELECT Year, STUSAB AS State, P0020002 AS Total, P0010002-P0020004 AS OneRace, P0010003-P0020005 AS White, P0010004-P0020006 AS Black, "
           "P0010005-P0020007 AS NativeAm, P0010006-P0020008 AS Asian, P0010007-P0020009 AS PacIslander, P0010008-P0020010 AS Other, "
-          "P0010009-P0020011 AS TwoOrMore FROM popdata2020 ORDER BY Total DESC").createOrReplaceTempView("hisp2020")
+          "P0010009-P0020011 AS TwoOrMore FROM popdata ORDER BY Total DESC").createOrReplaceTempView("hisp")
 
-hispUS2020 = spark.sql("SELECT MIN(Year) AS Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, "
-                       "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
-                       "SUM(TwoOrMore) AS TwoOrMore FROM hisp2020")
+hispUS = spark.sql("SELECT Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, SUM(NativeAm) AS NativeAm, "
+                   "SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, SUM(TwoOrMore) AS TwoOrMore FROM hisp "
+                   "GROUP BY Year")
 
-    # --------------- nonhispUS2020 (Non-Hispanic Totals for all categories) --------------- #
+    # --------------- nonhispUS (Non-Hispanic Totals for all categories) --------------- #
 spark.sql("SELECT Year, STUSAB AS State, P0020003 AS Total, P0020004 AS OneRace, P0020005 AS White, P0020006 AS Black, P0020007 AS NativeAm, "
-          "P0020008 AS Asian, P0020009 AS PacIslander, P0020010 AS Other, P0020011 AS TwoOrMore FROM popdata2020 ORDER BY Total DESC") \
-          .createOrReplaceTempView("nonhisp2020")
+          "P0020008 AS Asian, P0020009 AS PacIslander, P0020010 AS Other, P0020011 AS TwoOrMore FROM popdata ORDER BY Total DESC") \
+          .createOrReplaceTempView("nonhisp")
 
-nonhispUS2020 = spark.sql("SELECT MIN(Year) AS Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, "
-                          "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
-                          "SUM(TwoOrMore) AS TwoOrMore FROM nonhisp2020")
-
-# ---------------------------------------- 2010 DATA ---------------------------------------- #
-    # --------------- usData2010_1 (Totals for all categories) --------------- #
-spark.sql("SELECT Year, STUSAB AS State, P0010001 AS Total, P0010002 AS OneRace, P0010003 AS White, P0010004 AS Black, "
-          "P0010005 AS NativeAm, P0010006 AS Asian, P0010007 AS PacIslander, P0010008 AS Other, P0010009 AS TwoOrMore, "
-          "P0020002 AS Hispanic, P0020003 AS NonHispanic FROM popdata2010 ORDER BY Total DESC").createOrReplaceTempView("cat2010_1")
-
-usData2010_1 = spark.sql("SELECT MIN(Year) AS Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, "
-                         "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
-                         "SUM(TwoOrMore) AS TwoOrMore, SUM(Hispanic) AS Hispanic, SUM(NonHispanic) AS NonHispanic FROM cat2010_1")
-
-    # --------------- usData2010_2 (Non-Hispanic Totals for all categories, Hispanic as separate category) --------------- #
-spark.sql("SELECT Year, STUSAB AS State, P0010001 AS Total, P0020002 AS Hispanic, P0020005 AS White, P0020006 AS Black, P0020007 AS NativeAm, "
-          "P0020008 AS Asian, P0020009 AS PacIslander, P0020010 AS Other, P0020011 AS TwoOrMore FROM popdata2010 ORDER BY Total DESC") \
-          .createOrReplaceTempView("cat2010_2")
-
-usData2010_2 = spark.sql("SELECT MIN(Year) AS Year, SUM(Total) AS Total, SUM(Hispanic) AS Hispanic, SUM(White) AS White, SUM(Black) AS Black, "
-                         "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
-                         "SUM(TwoOrMore) AS TwoOrMore FROM cat2010_2")
-
-    # --------------- hispUS2010 (Hispanic Totals for all categories) --------------- #
-spark.sql("SELECT Year, STUSAB AS State, P0020002 AS Total, P0010002-P0020004 AS OneRace, P0010003-P0020005 AS White, P0010004-P0020006 AS Black, "
-          "P0010005-P0020007 AS NativeAm, P0010006-P0020008 AS Asian, P0010007-P0020009 AS PacIslander, P0010008-P0020010 AS Other, "
-          "P0010009-P0020011 AS TwoOrMore FROM popdata2010 ORDER BY Total DESC").createOrReplaceTempView("hisp2010")
-
-hispUS2010 = spark.sql("SELECT MIN(Year) AS Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, "
-                       "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
-                       "SUM(TwoOrMore) AS TwoOrMore FROM hisp2010")
-
-    # --------------- nonhispUS2010 (Non-Hispanic Totals for all categories) --------------- #
-spark.sql("SELECT Year, STUSAB AS State, P0020003 AS Total, P0020004 AS OneRace, P0020005 AS White, P0020006 AS Black, P0020007 AS NativeAm, "
-          "P0020008 AS Asian, P0020009 AS PacIslander, P0020010 AS Other, P0020011 AS TwoOrMore FROM popdata2010 ORDER BY Total DESC") \
-          .createOrReplaceTempView("nonhisp2010")
-
-nonhispUS2010 = spark.sql("SELECT MIN(Year) AS Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, "
-                          "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
-                          "SUM(TwoOrMore) AS TwoOrMore FROM nonhisp2010")
-
-# ---------------------------------------- 2000 DATA ---------------------------------------- #
-    # --------------- usData2000_1 (Totals for all categories) --------------- #
-spark.sql("SELECT Year, STUSAB AS State, P0010001 AS Total, P0010002 AS OneRace, P0010003 AS White, P0010004 AS Black, "
-          "P0010005 AS NativeAm, P0010006 AS Asian, P0010007 AS PacIslander, P0010008 AS Other, P0010009 AS TwoOrMore, "
-          "P0020002 AS Hispanic, P0020003 AS NonHispanic FROM popdata2000 ORDER BY Total DESC").createOrReplaceTempView("cat2000_1")
-
-usData2000_1 = spark.sql("SELECT MIN(Year) AS Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, "
-                         "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
-                         "SUM(TwoOrMore) AS TwoOrMore, SUM(Hispanic) AS Hispanic, SUM(NonHispanic) AS NonHispanic FROM cat2000_1")
-
-    # --------------- usData2000_2 (Non-Hispanic Totals for all categories, Hispanic as separate category) --------------- #
-spark.sql("SELECT Year, STUSAB AS State, P0010001 AS Total, P0020002 AS Hispanic, P0020005 AS White, P0020006 AS Black, P0020007 AS NativeAm, "
-          "P0020008 AS Asian, P0020009 AS PacIslander, P0020010 AS Other, P0020011 AS TwoOrMore FROM popdata2000 ORDER BY Total DESC") \
-          .createOrReplaceTempView("cat2000_2")
-
-usData2000_2 = spark.sql("SELECT MIN(Year) AS Year, SUM(Total) AS Total, SUM(Hispanic) AS Hispanic, SUM(White) AS White, SUM(Black) AS Black, "
-                         "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
-                         "SUM(TwoOrMore) AS TwoOrMore FROM cat2000_2")
-
-    # --------------- hispUS2000 (Hispanic Totals for all categories) --------------- #
-spark.sql("SELECT Year, STUSAB AS State, P0020002 AS Total, P0010002-P0020004 AS OneRace, P0010003-P0020005 AS White, P0010004-P0020006 AS Black, "
-          "P0010005-P0020007 AS NativeAm, P0010006-P0020008 AS Asian, P0010007-P0020009 AS PacIslander, P0010008-P0020010 AS Other, "
-          "P0010009-P0020011 AS TwoOrMore FROM popdata2000 ORDER BY Total DESC").createOrReplaceTempView("hisp2000")
-
-hispUS2000 = spark.sql("SELECT MIN(Year) AS Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, "
-                       "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
-                       "SUM(TwoOrMore) AS TwoOrMore FROM hisp2000")
-
-    # --------------- nonhispUS2000 (Non-Hispanic Totals for all categories) --------------- #
-spark.sql("SELECT Year, STUSAB AS State, P0020003 AS Total, P0020004 AS OneRace, P0020005 AS White, P0020006 AS Black, P0020007 AS NativeAm, "
-          "P0020008 AS Asian, P0020009 AS PacIslander, P0020010 AS Other, P0020011 AS TwoOrMore FROM popdata2000 ORDER BY Total DESC") \
-          .createOrReplaceTempView("nonhisp2000")
-
-nonhispUS2000 = spark.sql("SELECT MIN(Year) AS Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, "
-                          "SUM(NativeAm) AS NativeAm, SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, "
-                          "SUM(TwoOrMore) AS TwoOrMore FROM nonhisp2000")
-
-# -------------------- COMBINE AND REPARTITION DATAFRAMES -------------------- #
-usData_1 = usData2000_1.union(usData2010_1).union(usData2020_1).repartition(1) # usData_1 (Totals for all categories)
-usData_2 = usData2000_2.union(usData2010_2).union(usData2020_2).repartition(1) # usData_2 (Non-Hispanic Totals for all categories, Hispanic as separate category)
-hispUS = hispUS2000.union(hispUS2010).union(hispUS2020).repartition(1) # hispUS (Hispanic Totals for all categories)
-nonhispUS = nonhispUS2000.union(nonhispUS2010).union(nonhispUS2020).repartition(1) # nonhispUS (Non-Hispanic Totals for all categories)
+nonhispUS = spark.sql("SELECT Year, SUM(Total) AS Total, SUM(OneRace) AS OneRace, SUM(White) AS White, SUM(Black) AS Black, SUM(NativeAm) AS NativeAm, "
+                      "SUM(Asian) AS Asian, SUM(PacIslander) AS PacIslander, SUM(Other) AS Other, SUM(TwoOrMore) AS TwoOrMore FROM nonhisp "
+                      "GROUP BY Year")
 
 # ---------- UNCOMMENT TO VERIFY DATAFRAMES ---------- #
 usData_1.show()
@@ -167,11 +90,11 @@ hispUS.show()
 print("Non-Hispanic Population")
 nonhispUS.show()
 
-# # --------------- SAVE FILES --------------- #
-# savepath = path + "query_data/byCategory/"
+# --------------- SAVE FILES --------------- #
+savepath = path + "query_data/byCategory/"
 # usData_1.write.csv(savepath + "usData_1", header=True)
 # usData_2.write.csv(savepath + "usData_2", header=True)
-# hispUS.write.csv(savepath + "hispUS", header=True)
+hispUS.write.csv(savepath + "hispUS", header=True)
 # nonhispUS.write.csv(savepath + "nonhispUS", header=True)
 
 # TO MAKE QUERIES EASIER, RENAME THE RESULTING CSV FILES TO THE NAMES OF THE DATAFRAMES THEY WERE
