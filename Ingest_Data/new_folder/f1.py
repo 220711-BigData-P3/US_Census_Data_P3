e0556987b3b0c0d41c5d2a4a4dd4f5cc3ce0a46f
SJT1988 <strumbore88@gmail.com>
2022-09-08 08:11:25 -0700

    empty file cleanup

--- Ingest_Data/new_folder/f1.py
+++
@@ -1,5 +0,0 @@
-class f1:
-
-    def write_csv_summaries():
-        print('this was f1.')
-        return
--- Ingest_Data/new_folder/f2.py
+++
@@ -1,4 +0,0 @@
-class f2:
-    def upload():
-        print('this was f2.')
-        return
