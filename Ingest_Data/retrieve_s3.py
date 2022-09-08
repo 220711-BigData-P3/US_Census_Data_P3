269b4f486a19c1f1a65eb61744910a8bb3d557b0
justinchore <jchoustin91@gmail.com>
2022-09-08 08:39:24 -0700

    Retrieve s3 Edited, added deletion function

--- Ingest_Data/retrieve_s3.py
+++ Ingest_Data/retrieve_s3.py
@@ -22,17 +22,33 @@ client = session.client('s3')
 #fetch bucket from resource
 bucket = resource.Bucket(bucket_name)
 
+
+################S3 DOWNLOAD, CHECK CONTENTS, DELETION FUNCTIONS HERE!####################
+
+
 def download_from_s3(bucket):
-    #Download file into local
+    #Downloads file into local
     for item in bucket.objects.all():
-        print(item.key)
         with open(item.key, 'wb') as f:
             client.download_fileobj(item.bucket_name, item.key, f)
             
+            
 def check_bucket_contents(bucket):
-    for item in bucket.objects.all():
-        print(item.key)
+    bucket_objects = bucket.objects.all()
+    names = map(lambda summary: summary.key, bucket_objects)
+    return names
         
+def delete_file_from_bucket(bucketname, bucket, filename):
+    names = check_bucket_contents(bucket)
+    if filename not in names:
+        print(f"Error: File: {filename} doesn't exist.")
+        return 
+    resource.Object(bucketname, filename).delete()
+    print(f"Succesfully deleted {filename}!")
+    return 
 
-#check_bucket_contents(bucket)
+
+check_bucket_contents(bucket)
+delete_file_from_bucket(bucket_name, bucket,'tgjksdyhfgkldshjfg', )
+check_bucket_contents(bucket)
 #download_from_s3(bucket)
