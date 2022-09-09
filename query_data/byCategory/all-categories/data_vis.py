a04fc1193508dbe4aa0ba53705266acd6805a0f9
jnesteruck <jan9499@gmail.com>
2022-09-09 12:19:13 -0700

    updates to pie chart, legend, updates to code

--- query_data/byCategory/all-categories/data_vis.py
+++ query_data/byCategory/all-categories/data_vis.py
@@ -44,6 +44,47 @@ df2 = pd.read_csv(path + "nonhispUS.csv", index_col="Year")[races]
 df1.columns = hisp # Rename columns
 df2.columns = nonhisp # Rename columns
 allCats = df1.join(df2).transpose() # Want Years as columns
+
+# PIE CHARTS
+def autopct(pct):
+    if pct > 10:
+        return f'{pct:.1f}%'
+    elif pct > 2:
+        return f'{pct:.2f}%'
+    else:
+        return ''
+
+# LEGEND
+leg = allCats.plot.pie(y=2000, figsize=(15,6))
+plt.legend(title = 'Key', bbox_to_anchor=(-0.1, 0.8))
+
+# COLOR ORDER TO MATCH LEGEND
+allCats2010 = ['tab:blue','tab:orange','tab:green','tab:red','tab:purple','tab:brown','tab:pink',
+	           'tab:grey','tab:olive','tab:blue','tab:cyan','tab:orange','tab:green','tab:red']
+allCats2020 = ['tab:blue','tab:orange','tab:red','tab:pink','tab:purple','tab:green','tab:brown',
+	           'tab:grey','tab:cyan','tab:blue','tab:olive','tab:orange','tab:green','tab:red']
+               
+# SET PLOT SIZE
+size = (10,8)
+
+# 2000
+allCats.sort_values(by=[2000], inplace=True, ascending=False) # Sort by 2020 values
+ax = allCats.plot.pie(y=2000, labels=None, figsize=size, autopct=autopct, fontsize=12, title="2000", legend=False)
+y_axis = ax.axes.get_yaxis()
+y_axis.set_visible(False) # Remove title from y-axis side of plot
+
+# 2010
+allCats.sort_values(by=[2010], inplace=True, ascending=False)
+ax = allCats.plot.pie(y=2010, labels=None, figsize=size, autopct=autopct, fontsize=12, colors= allCats2010, title="2010", legend=False)
+y_axis = ax.axes.get_yaxis()
+y_axis.set_visible(False)
+
+# 2010
+allCats.sort_values(by=[2020], inplace=True, ascending=False)
+ax = allCats.plot.pie(y=2020, labels=None, figsize=size, autopct=autopct, fontsize=12, colors= allCats2020, title="2020", legend=False)
+y_axis = ax.axes.get_yaxis()
+y_axis.set_visible(False)
+
 # RENAME COLUMNS FOR OPERATIONS
 allCats.columns = ["C2000", "C2010", "C2020"]
 # CREATE NEW COLUMNS TO SHOW CHANGE B/W CENSUSES
