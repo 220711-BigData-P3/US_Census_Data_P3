474c4cb10020cc475deb5aa9d9378047746240af
justinchore <jchoustin91@gmail.com>
2022-09-10 23:28:04 -0700

    Prediction Table Built

--- query_data/Justin/queries.py
+++ query_data/Justin/queries.py
@@ -1,6 +1,6 @@
 import os
 from pyspark.sql import SparkSession
-import pyspark.sql.functions
+from pyspark.sql.functions import col
 
 
 #Create SparkSession
@@ -14,4 +14,116 @@ state_abbrevs = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI
 
 
 #Path Building
-project_root_path = os.getcwd().split('Us_Census_Data')[0] + 'Us_Census_Data'
+project_root_path = "file:/mnt/c/Users/jchou/Desktop/Us_Census_Data_P3/"
+print(project_root_path)
+
+#######################VIEWS##################
+#Creating TempViews
+filenames = ['2000_1.csv', '2010_1.csv', '2020_P1.csv']
+
+def createTempViews():
+    for file in filenames:
+        year = file.split("_")[0]
+        filepath = project_root_path + f'/{file}'
+        print(filepath)
+        df = spark.read.option('header', True).option("inferSchema", True).csv(filepath)
+        filtered_df = df.select(col("STUSAB"), col("P0010001").alias(f"population")).filter(col("STUSAB").isin(*state_abbrevs))
+        filtered_df.createOrReplaceTempView(f"{file.split('.')[0]}_view")
+        
+createTempViews()
+spark.sql('SHOW VIEWS').show()
+'''
+    Output of show():
+        +---------+------------+-----------+
+        |namespace|    viewName|isTemporary|
+        +---------+------------+-----------+
+        |         | 2000_1_view|       true|
+        |         | 2010_1_view|       true|
+        |         |2020_p1_view|       true|
+        +---------+------------+-----------+
+    
+'''
+    
+#Join 2000, 2010 views on "STUSAB" into 1 dataframe
+populations = spark.sql("""
+                      SELECT 
+                        v1.STUSAB as State,
+                        v1.population AS 2000_population,
+                        v2.population AS 2010_population,
+                        v3.population AS 2020_population
+                      FROM 
+                        2000_1_view AS v1 
+                        JOIN 2010_1_view AS v2
+                        JOIN 2020_P1_view AS v3
+                      ON 
+                        v1.STUSAB = v2.STUSAB
+                      AND v1.STUSAB = v3.STUSAB  
+                      """)
+
+print(populations.count()) #50
+'''
+    Sample show of populations dataframe:
+    
+        |State|2000_population|2010_population|
+        +-----+---------------+---------------+
+        |   AK|         626932|         710231|
+        |   AL|        4447100|        4779736|
+        |   AR|        2673400|        2915918|
+        |   AZ|        5130632|        6392017|
+        |   CA|       33871648|       37253956|
+        |   CO|        4301261|        5029196|
+        |   CT|        3405565|        3574097|
+        |   DE|         783600|         897934|
+        |   FL|       15982378|       18801310|
+        
+'''
+
+#Create view of joined populations table
+
+populations.createOrReplaceTempView("populations")
+# populations.show()
+
+
+#Use analytic function on each row to calculate the percentage of increase/decrease
+'''
+#Finding percentage increase/decrease between 2 points
+    Using AK data
+        point2000 = 626932
+        point2010 = 710231
+        
+        1. Subtract point2000 from point2010: 710231 - 626932
+        2. Divide the amount by point 2010 (decimal) ROUND (2) * 100 for percentage
+'''
+
+populations_with_change = spark.sql("""
+                                    SELECT 
+                                        
+                                        2000_population,
+                                        2010_population,
+                                        ((2010_population - 2000_population) / 2010_population) AS 2000_to_2010_change,
+                                        (CASE
+                                            WHEN 2000_population > 2010_population THEN "Decrease"
+                                            WHEN 2000_population < 2010_population THEN "Increase"
+                                            ELSE "No Change"
+                                        END) as 2000_to_2010_direction,
+                                        2020_population,
+                                        (CASE
+                                            WHEN 2010_population > 2020_population THEN "Decrease"
+                                            WHEN 2010_population < 2020_population THEN "Increase"
+                                            ELSE "No Change"
+                                        END) as 2010_to_2020_direction
+                                        
+                                    FROM 
+                                        populations
+                                    """)
+
+populations_with_change.show(50)
+prediction_comparison = spark.sql("""
+                                  
+                                  
+                                  
+                                  """)
+
+
+
+
