8739b18d8c680e669332ebc033d3bd1486f7e0b6
Justin Cho <jchoustin91@gmail.com>
2022-09-10 21:06:19 -0700

    Prediction Start

---
+++ query_data/Justin/queries.py
@@ -0,0 +1,16 @@
+import os
+from pyspark.sql import SparkSession
+import pyspark.sql.functions
+
+
+#Create SparkSession
+spark = SparkSession.builder\
+            .master("local")\
+            .appName("2020_population_prediction")\
+            .getOrCreate()
+
+            
+state_abbrevs = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
+
+
+print(os.getcwd().split('Us_Census_Data')[0])
